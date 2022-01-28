9e7f35ab472adf792e425e0422772d6afec7646d
Ben Axnick <ben.axnick@zendesk.com>
2022-01-28 15:54:47 +1100

    We did a thing!
    Can now iterate the board state

--- cmd/game_of_life/main.go
+++ cmd/game_of_life/main.go
@@ -1,8 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func Board(dx, dy int) [][]uint8 {
+func Board(dy, dx int) [][]uint8 {
 	board := make([][]uint8, dx)
 	cells := make([]uint8, dx*dy)
 
@@ -38,19 +40,89 @@ func DrawBoard(board [][]uint8) {
 	}
 }
 
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+func CountLiveNeighbours(board [][]uint8, y, x int) int {
+	x1 := Max(x-1, 0)
+	x2 := Min(x+1, len(board[y])-1)
+	y1 := Max(y-1, 0)
+	y2 := Min(y+1, len(board)-1)
+
+	count := 0
+
+	for y := y1; y <= y2; y++ {
+		for x := x1; x <= x2; x++ {
+			if board[y][x] == 1 {
+				count++
+			}
+		}
+	}
+
+	if board[y][x] == 1 {
+		count--
+	}
+
+	return count
+}
+
+func NextCellState(board [][]uint8, y, x int) uint8 {
+	isLive := board[y][x] == 1
+	neighbours := CountLiveNeighbours(board, y, x)
+
+	if isLive && neighbours == 2 || neighbours == 3 {
+		return 1
+	} else if !isLive && neighbours == 3 {
+		return 1
+	}
+
+	return 0
+}
+
+func UpdateBoard(board [][]uint8) {
+	next_board := Board(len(board), len(board[0]))
+
+	for y := range board {
+		for x := range board[y] {
+			next_board[y][x] = NextCellState(board, y, x)
+		}
+	}
+
+	copy(board, next_board)
+}
+
 func main() {
 	// Initialise a zeroed game board
 	board := Board(5, 5)
-	tick := 1
+	tick := 0
 
 	// Activate some pixels, probably start with something fixed
 	SeedBoard(board)
 
 	// Display the board, probably just terminal based for starters
+	tick++
 	fmt.Printf("Tick %d\n", tick)
 	DrawBoard(board)
 
 	// Function to calculate next board state from current board state
+	tick++
+	fmt.Printf("Tick %d\n", tick)
+	UpdateBoard(board)
+	DrawBoard(board)
+
 	// Game loop to calculate next game state, display the board
 
 	// That's the basics out of the way. Other thoughts:
