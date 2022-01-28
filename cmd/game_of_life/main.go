15802fe29dbfec4df056678b436ee9bb9c61712e
Ben Axnick <ben.axnick@zendesk.com>
2022-01-28 13:42:41 +1100

    Implement barebones functions to init, seed and display a board

--- cmd/game_of_life/main.go
+++ cmd/game_of_life/main.go
@@ -2,12 +2,54 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello, world!")
+func Board(dx, dy int) [][]uint8 {
+	board := make([][]uint8, dx)
+	cells := make([]uint8, dx*dy)
 
+	for y := range board {
+		board[y], cells = cells[:dy], cells[dy:]
+	}
+
+	return board
+}
+
+func SeedBoard(board [][]uint8) {
+	board[1][1] = 1
+	board[1][3] = 1
+	board[3][1] = 1
+	board[3][2] = 1
+	board[3][3] = 1
+}
+
+func DrawBoard(board [][]uint8) {
+	for y := range board {
+		row := make([]rune, len(board[y]))
+
+		for x := range board[y] {
+			switch board[y][x] {
+			case 1:
+				row[x] = '😺'
+			case 0:
+				row[x] = '🖤'
+			}
+		}
+
+		fmt.Println(string(row))
+	}
+}
+
+func main() {
 	// Initialise a zeroed game board
+	board := Board(5, 5)
+	tick := 1
+
 	// Activate some pixels, probably start with something fixed
+	SeedBoard(board)
+
 	// Display the board, probably just terminal based for starters
+	fmt.Printf("Tick %d\n", tick)
+	DrawBoard(board)
+
 	// Function to calculate next board state from current board state
 	// Game loop to calculate next game state, display the board
 
