6fa501cc568e7adccd29e7d786d5b4afb078d98f
Ben Axnick <ben.axnick@zendesk.com>
2022-01-28 13:03:52 +1100

    Commented some ideas about the rough order of implementation

--- cmd/game_of_life/main.go
+++ cmd/game_of_life/main.go
@@ -4,4 +4,22 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, world!")
+
+	// Initialise a zeroed game board
+	// Activate some pixels, probably start with something fixed
+	// Display the board, probably just terminal based for starters
+	// Function to calculate next board state from current board state
+	// Game loop to calculate next game state, display the board
+
+	// That's the basics out of the way. Other thoughts:
+
+	// Unit test the calculation of next game state if we haven't already
+	// Use Ebiten to draw the game board
+	// Add play/pause toggle
+	// Add clear/reset toggle
+	// Add a speed slider
+	// Toggle cells with mouse click
+	// Implement some additional seeds, have a picker to load the chosen seed
+	// Add a way to save a seed from the current game state
+	// Deploy game to a website using WASM
 }
